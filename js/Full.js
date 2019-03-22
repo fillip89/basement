bd2e32e1ae386d1cc82f5006a5953641769f1c0d
fillip89 <35250025+fillip89@users.noreply.github.com>
2019-03-23 05:15:03 +0900

    Add files via upload

---
+++ js/Full.js
@@ -0,0 +1,65 @@
+var divsToHide = document.getElementsByClassName("Club"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("Karaoke"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("ShirtRoom"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("TenCafe"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("Dot5"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("Bar"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+/*var divsToHide = document.getElementsByClassName("Full"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}*/
+
+var divsToHide = document.getElementsByClassName("Hard"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("Massage"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("OP"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
+
+var divsToHide = document.getElementsByClassName("HostBar"); //divsToHide is an array
+for(var i = 0; i < divsToHide.length; i++){
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
